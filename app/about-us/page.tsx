c167a8efcf22f9a210c2b9d8a5b5c31e910dcfc8
Park JunYong <dyden119@gmail.com>
2024-11-04 22:07:41 +0900

    docs: first commit

---
+++ app/about-us/page.tsx
@@ -0,0 +1,10 @@
+import Navigation from "../../components/navigation";
+
+export default function AboutUs() {
+  return (
+    <main>
+      <h1>About Us</h1>
+      <Navigation />
+    </main>
+  );
+}
