f1dd2bfcb72484d74e47a7a99fde56316905f74c
Park JunYong <dyden119@gmail.com>
2024-11-09 00:32:23 +0900

    chore: 동적 메타데이터

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,14 @@
 import { Suspense } from "react";
-import { API_URL } from "../../../(home)/page";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
+
+export async function generateMetadata({ params }) {
+  const { id } = await params;
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
 
 export default async function MovieDetail({ params }) {
   const { id } = await params;
